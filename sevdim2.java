c97f08a658b741e2f3ae51709f83cbbbe951360b
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2023-11-28 15:22:50 +1100

    Add files via upload

---
+++ sevdim2.java
@@ -0,0 +1,30 @@
+public class sevdim2
+ {
+      public static void main(String[] args) {
+             int i;
+             int j;
+             int k;
+             int l;
+             int m;
+             int n;
+             int o;
+             int [][][][][][][] array1 = new int[7][7][7][7][7][7][7];
+             for(i=1;i<=6;i++){
+              for(j=1;j<=6;j++){
+               for(k=1;k<=6;k++){
+                for(l=1;l<=6;l++){
+                 for(m=1;m<=6;m++){
+                  for(n=1;n<=6;n++){
+                   for(o=1;o<=6;o++){
+
+                      array1[o][n][m][l][k][j][i] = 1;
+                     System.out.printf("\n  %d  %d %d  %d  %d  %d  %d",o,n,m,l,k,j,i);
+                                      }
+                    }
+                   }
+                  }
+                  }
+                 }
+                 }
+                 }
+                 }
